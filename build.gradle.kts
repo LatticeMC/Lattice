a3108a2b81deebd444fbb67ec08646f3ac29e39e
sweepikun <laomgba@outlook.com>
2026-06-13 14:07:25 +0800

    fix: add spongepowered maven for mixin resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,6 +50,7 @@ subprojects {
     repositories {
         mavenCentral()
         maven(paperMavenPublicUrl)
+        maven("https://repo.spongepowered.org/repository/maven-public/")
     }
 
     tasks.withType<AbstractArchiveTask>().configureEach {
